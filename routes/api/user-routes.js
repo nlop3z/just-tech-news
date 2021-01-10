e84004167cad6dfb93670b27ca1d725fcb86c0db
nlopez <nelly.lopez@utexas.edu>
2021-01-10 16:59:27 -0600

    fixed typo in user.js

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,105 +3,108 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll({
-      attributes: { exclude: ['password'] }
-    })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-//We just set up the API endpoint so that when the client makes a GET request to /api/users, we will select all users from the user table in the database and send it back as JSON.
-// statement above is like SQL query: SELECT * FROM users WHERE id = 1
-// GET /api/users/1
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-      // statement above is like SQL query: SELECT * FROM users WHERE id = 1
-    }
+  // Access our User model and run .findAll() method)
+  User.findAll({
+    attributes: { exclude: ['password'] }
   })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// POST /api/users
-router.post('/', (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-    // in SQL it would be code below:
-//     INSERT INTO users
-//   (username, email, password)
-//   VALUES
-//     ("Lernantino", "lernantino@gmail.com", "password1234");
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
-});
+  });
+  //We just set up the API endpoint so that when the client makes a GET request to /api/users, we will select all users from the user table in the database and send it back as JSON.
+  // statement above is like SQL query: SELECT * FROM users WHERE id = 1
 
-// PUT /api/users/1
-router.put('/:id', (req, res) => {
+  // GET /api/users/1
+  router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+        // statement above is like SQL query: SELECT * FROM users WHERE id = 1
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // POST /api/users
+  router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      // in SQL it would be code below:
+      //     INSERT INTO users
+      //   (username, email, password)
+      //   VALUES
+      //     ("Lernantino", "lernantino@gmail.com", "password1234");
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // PUT /api/users/1
+  router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
-  User.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  })
-//   Associated SQL syntax would be below:
-//   UPDATE users
-// SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
-// WHERE id = 1;
-    .then(dbUserData => {
-      if (!dbUserData[0]) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    User.update(req.body, {
+      where: {
+        id: req.params.id
       }
-      res.json(dbUserData);
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+      //   Associated SQL syntax would be below:
+      //   UPDATE users
+      // SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
+      // WHERE id = 1;
+      .then(dbUserData => {
+        if (!dbUserData[0]) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
-// DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+  // DELETE /api/users/1
+  router.delete('/:id', (req, res) => {
     router.delete('/:id', (req, res) => {
-        User.destroy({
-          where: {
-            id: req.params.id
+      User.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
           }
+          res.json(dbUserData);
         })
-          .then(dbUserData => {
-            if (!dbUserData) {
-              res.status(404).json({ message: 'No user found with this id' });
-              return;
-            }
-            res.json(dbUserData);
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-          });
-});
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+  });
 
-module.exports = router;
+    module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // This file, like index in models folder will serve as a means to collect all of the API routes and package them up for us
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./api/user-routes.js');
 
 router.use('/users', userRoutes);
 
