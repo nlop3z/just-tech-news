31d9f6abd605bee2a33b833c416738b3012dcd6c
nlopez <nelly.lopez@utexas.edu>
2021-01-10 20:02:31 -0600

    added instance to compare user passwords in login process and created login route for authentication

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -48,19 +53,18 @@ User.init(
     },
     {
       hooks: {
-         async beforeCreate(newUserData) {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
           newUserData.password = await bcrypt.hash(newUserData.password, 10);
-            return newUserData
-          },
-          async beforeUpdate(updatedUserData) {
-            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-            return updatedUserData;
-          }
-        
+          return newUserData;
         },
   
-      
-      // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration)
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
+      //table config options go here
       sequelize,
       timestamps: false,
       freezeTableName: true,
@@ -68,5 +72,6 @@ User.init(
       modelName: 'user'
     }
   );
-
-module.exports = User;
+  
+  module.exports = User;
+  
